a8b4fb00bc5a744ce42f5f1d43a9e2965b3103ed
Alice Smith <alice@webage.com>
2025-07-09 09:55:27 -0400

    updated validator to take in a bmodel in the constructor

--- server/validator.py
+++ server/validator.py
@@ -1,22 +1,35 @@
 import pandas as pd
 from processor import PProcessor
 import joblib
-import json
+import warnings
 from pydantic import BaseModel
 
 
 class Validator:
-    """The following class serves as a validator
-    to confirm whether unsafe user inputs are
-    properly formatted and converts the input
-    features into format which a given model
-    can use."""
-    def __init__(self,proc_pkl,model_pkl):
+    """The following class serves as a validator to confirm whether unsafe user inputs are
+    properly formatted and converts the input features into format which a given model
+    can use.
+    
+    This class must take in a valid ptransformer.pkl file and a valid model.pkl file.
+    A pydantic BaseModel is optional but highly recommended.
+    """
+    def __init__(self,proc_pkl,model_pkl,bmodel:BaseModel=None) -> None:
         self.pproc = PProcessor().load(proc_pkl)
         self.model = joblib.load(str(model_pkl))
+        self.bmodel = bmodel
     
-    def _uvalidate(self,input:dict,bmodel:BaseModel=None) -> pd.DataFrame:
-        return input
+    def _uvalidate(self,uinput:dict) -> pd.DataFrame:
+        """validates the given user input based on the given BaseModel"""
+        if self.bmodel:
+            # given base model to validate input
+            midata = self.bmodel(**uinput)
+            return pd.DataFrame.from_dict(midata.model_dump(),orient='index').transpose().convert_dtypes()
+        else:
+            # ignore data validation and try to jump to predictions
+            # this is not recommended but the model will always throw 
+            # an error given bad data.
+            warnings.warn("this is NOT recommended, to suppress warning add a valid BaseModel")
+            return pd.DataFrame.from_dict(uinput,orient='index').transpose().convert_dtypes()
 
     def run(self, uinput) -> dict:
         v_df = self._uvalidate(uinput)
